e010fa79924043d6a75c333fe000b4a9252264be
Paulo Rodrigues <paulo.jorge.rodrigues@gmail.com>
2025-05-31 14:24:47 +0100

    Solution

--- app/main.py
+++ app/main.py
@@ -1,5 +1,11 @@
 def format_linter_error(error: dict) -> dict:
-    return {"line": error.line_number, "column": error.column_number, "message": error.message, "name": error.name, "source": error.source}
+    return {
+        "line": error.line_number,
+        "column": error.column_number,
+        "message": error.message,
+        "name": error.name,
+        "source": error.source
+    }
 
 
 def format_single_linter_file(file_path: str, errors: list) -> dict:
@@ -14,5 +20,7 @@ def format_single_linter_file(file_path: str, errors: list) -> dict:
 
 def format_linter_report(linter_report: dict) -> list:
     return [
-        format_single_linter_file(file, errors) for file, errors in linter_report.items()
+        format_single_linter_file(
+            file, errors
+        ) for file, errors in linter_report.items()
     ]
